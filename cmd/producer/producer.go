66940e71b01f63ff1c74c576a69faf0d97451c70
nutcas3 <mauricee423@gmail.com>
2024-05-24 12:55:13 +0300

    helper function

--- cmd/producer/producer.go
+++ cmd/producer/producer.go
@@ -19,3 +19,23 @@ const (
 	KafkaServerAddress = "localhost:9092"
 	KafkaTopic         = "notifications"
 )
+
+var ErrUserNotFoundInProducer = errors.New("user not found")
+
+func findUserByID(id int, users []models.User) (models.User, error) {
+	for _, user := range users {
+		if user.ID == id {
+			return user, nil
+		}
+	}
+	return models.User{}, ErrUserNotFoundInProducer
+}
+
+func getIDFromRequest(formValue string, ctx *gin.Context) (int, error) {
+	id, err := strconv.Atoi(ctx.PostForm(formValue))
+	if err != nil {
+		return 0, fmt.Errorf(
+			"failed to parse ID from form value %s: %w", formValue, err)
+	}
+	return id, nil
+}
