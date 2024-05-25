5ef0752cdb3841b942a0a669203d3976c41cfcb4
nutcas3 <mauricee423@gmail.com>
2024-05-25 11:42:37 +0300

    Feat :Producer Golang 	- function for send message handler from user 	- function to setup producer 	- main function to run the producer of kafka

--- cmd/producer/producer.go
+++ cmd/producer/producer.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -72,4 +72,70 @@ func sendKafkaMessage(producer sarama.SyncProducer,
 
 	_, _, err = producer.SendMessage(msg)
 	return err
+}
+func sendMessageHandler(producer sarama.SyncProducer,
+	users []models.User) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		fromID, err := getIDFromRequest("fromID", ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		toID, err := getIDFromRequest("toID", ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		err = sendKafkaMessage(producer, users, ctx, fromID, toID)
+		if errors.Is(err, ErrUserNotFoundInProducer) {
+			ctx.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+			return
+		}
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Notification sent successfully!",
+		})
+	}
+}
+func setupProducer() (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	producer, err := sarama.NewSyncProducer([]string{KafkaServerAddress},
+		config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to setup producer: %w", err)
+	}
+	return producer, nil
+}
+
+func main() {
+	users := []models.User{
+		{ID: 1, Name: "Emmax"},
+		{ID: 2, Name: "Bradoxx"},
+		{ID: 3, Name: "Ricky"},
+		{ID: 4, Name: "Wagwan"},	
+	}
+	producer, err := setupProducer()
+	if err != nil {
+		log.Fatalf("failed to initialize producer: %v", err)	
+	}
+	defer producer.Close()
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.POST("/send",sendMessageHandler(producer,users))
+
+	fmt.Printf("Kafka PRODUCER started at http://localhost%s\n",
+		ProducerPort)
+	if err := router.Run(ProducerPort); err != nil{
+		log.Printf("failed to run the server: %v", err)
+	}
 }
